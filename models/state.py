3e0d7b9cc09d2de28d33acd2944a056a96be1a45
Eduardo Figueroa <123639170+semaed@users.noreply.github.com>
2023-10-18 14:52:28 -0400

    Updating state.py
    Adding comments to the state code file

--- models/state.py
+++ models/state.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module containing the state
+"""Module containing the State
 """
 
 from models.base_model import BaseModel
 
-
+# Define a class called State that inherits from BaseModel
 class State(BaseModel):
     """ State class that inherits from BaseModel """
-    name = ""
+    name = ""  # Define an attribute called 'name' for a state
